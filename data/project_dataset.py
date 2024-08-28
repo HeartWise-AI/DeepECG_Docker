3b578f1bf2c3c958e3854fa6c604cfc6af3d0acb
JacquesDelfrate <jdelfrate504@gmail.com>
2024-08-28 11:47:09 +0000

    one dataset to rule them all

--- data/project_dataset.py
+++ data/project_dataset.py
@@ -7,18 +7,20 @@ class ProjectDataset:
     def __init__(self, df: pd.DataFrame):
         self.diagnosis = df['diagnosis']
         self.npy_path = df['npy_path']
+        self.df = df
     def __len__(self):
         return len(self.diagnosis)
     
     def __getitem__(self, idx):
         ecg_signal = np.load(self.npy_path.iloc[idx]).squeeze(-1)
         ecg_signal = ecg_signal.transpose(1, 0)
-        return self.diagnosis.iloc[idx], torch.from_numpy(ecg_signal).float()  
+        labels = self.df.iloc[idx][2:].astype(float).values
+        return self.diagnosis.iloc[idx], torch.from_numpy(ecg_signal).float(), labels
 
-def create_dataloader(df: pd.DataFrame):
+def create_dataloader(df: pd.DataFrame, batch_size: int = 1):
     dataset = ProjectDataset(df)
 
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     return dataloader
 
 
