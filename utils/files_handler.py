f58eb524215bb54363511ec517cf5d9ceee69519
JacquesDelfrate <jdelfrate504@gmail.com>
2024-08-30 15:44:58 +0000

    files handler routines went to files_handler

---
+++ utils/files_handler.py
@@ -0,0 +1,20 @@
+import os
+import csv
+import json
+
+def save_to_csv(metrics: dict, path: str) -> None:
+    if not os.path.exists('/'.join(path.split('/')[:-1])):
+        os.makedirs('/'.join(path.split('/')[:-1]))
+
+    # Open the file and create a CSV writer
+    with open(path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for metric in metrics:
+            for value in metrics[metric]:
+                writer.writerow([metric, value, metrics[metric][value]])
+
+def save_to_json(data: dict, path: str) -> None:
+    if not os.path.exists('/'.join(path.split('/')[:-1])):
+        os.makedirs('/'.join(path.split('/')[:-1]))
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
