5cd41008d6b3a4479c7d083c5b06b8303ec7e153
JacquesDelfrate <jdelfrate504@gmail.com>
2024-08-30 15:45:23 +0000

    rename models_factory to hearwise_models_factory because it sounds cooler

--- models/heartwise_model_factory.py
+++ models/heartwise_model_factory.py
@@ -1,17 +1,15 @@
 
 
-
-
-
-class ModelFactory:
+class HeartWiseModelFactory:
     @classmethod
     def create_model(cls, model_config):
         model_name = model_config['model_name']
         model_name_lower = model_name.lower()
+        print(cls.__subclasses__())
         for subclass in cls.__subclasses__():
             if subclass.name.lower() == model_name_lower:
                 return subclass(**model_config)
-        raise ValueError(f"Model {model_name} not found")
+        raise ValueError(f"Model {model_name} not found\nAvailable models: {[subclass.name for subclass in cls.__subclasses__()]}")
 
 
 
