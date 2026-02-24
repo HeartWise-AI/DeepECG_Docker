e63ce059a405f8a8591135923cf06db6948a4c55
otastet <o.tastet33@gmail.com>
2026-02-24 09:47:03 -0500

    feat: add ecg_processing_mode parameter to run_pipeline.bash for enhanced flexibility

--- run_pipeline.bash
+++ run_pipeline.bash
@@ -26,6 +26,7 @@ ecg_signals_path=$(get_param "ecg_signals_path")
 mode=$(get_param "mode")
 preprocessing_folder=$(get_param "preprocessing_folder")
 preprocessing_n_workers=$(get_param "preprocessing_n_workers")
+ecg_processing_mode=$(get_param "ecg_processing_mode")
 
 # Overwrite mode by command line argument
 # Parse command-line arguments
@@ -47,6 +48,16 @@ while [[ "$#" -gt 0 ]]; do
                 shift 2
             else
                 echo "Error: --csv_file requires a non-empty argument."
+                usage
+                return 1
+            fi
+            ;;
+        --ecg_processing_mode)
+            if [[ -n $2 && ! $2 =~ ^-- ]]; then
+                ecg_processing_mode="$2"
+                shift 2
+            else
+                echo "Error: --ecg_processing_mode requires a non-empty argument (auto, single, batch)."
                 usage
                 return 1
             fi
@@ -79,7 +90,8 @@ run_pipeline() {
         --ecg_signals_path $ecg_signals_path \
         --mode $run_mode \
         --preprocessing_folder $preprocessing_folder \
-        --preprocessing_n_workers $preprocessing_n_workers
+        --preprocessing_n_workers $preprocessing_n_workers \
+        --ecg_processing_mode $ecg_processing_mode
 }
 
 # Main execution based on mode
